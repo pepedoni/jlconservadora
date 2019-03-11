0e47af5e73d97d41882c64a62a6ba92c1b23c385
pepedoni <phpedromoutinho@gmail.com>
2019-03-10 21:54:26 -0300

    AutoComplete
    AutoComplete

--- frontend/src/core/_input/autoComplete.tsx
+++ frontend/src/core/_input/autoComplete.tsx
@@ -1,92 +1,268 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import deburr from 'lodash/deburr';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
 import JlInput from './input';
-import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {
-    AutoComplete,
-} from 'antd';
-import './autoComplete.less';
-import { CLIENT_SAVE_SUCCESS } from '../../features/client/constants';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import request from '../../api/request';
 
-const Option = AutoComplete.Option;
+const suggestions = [
+  { label: 'Afghanistan' },
+  { label: 'Aland Islands' },
+  { label: 'Albania' },
+  { label: 'Algeria' },
+  { label: 'American Samoa' },
+  { label: 'Andorra' },
+  { label: 'Angola' },
+  { label: 'Anguilla' },
+  { label: 'Antarctica' },
+  { label: 'Antigua and Barbuda' },
+  { label: 'Argentina' },
+  { label: 'Armenia' },
+  { label: 'Aruba' },
+  { label: 'Australia' },
+  { label: 'Austria' },
+  { label: 'Azerbaijan' },
+  { label: 'Bahamas' },
+  { label: 'Bahrain' },
+  { label: 'Bangladesh' },
+  { label: 'Barbados' },
+  { label: 'Belarus' },
+  { label: 'Belgium' },
+  { label: 'Belize' },
+  { label: 'Benin' },
+  { label: 'Bermuda' },
+  { label: 'Bhutan' },
+  { label: 'Bolivia, Plurinational State of' },
+  { label: 'Bonaire, Sint Eustatius and Saba' },
+  { label: 'Bosnia and Herzegovina' },
+  { label: 'Botswana' },
+  { label: 'Bouvet Island' },
+  { label: 'Brazil' },
+  { label: 'British Indian Ocean Territory' },
+  { label: 'Brunei Darussalam' },
+];
 
-const styles = theme => ({
-    filter: {},
-    default: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-    }
-});
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+  const matches = match(suggestion.address_district, query);
+  const parts = parse(suggestion.address_district, matches);
 
-class JlAutoComplete extends Component {
-
-    constructor(props) {
-
-        super(props);
-        
-        this.state = {
-            data: []
-        };
-
-        this.getTextItem = this.getTextItem.bind(this);
-        this.renderOption = this.renderOption.bind(this);
-        this.setData       = this.setData.bind(this);
-        this.setData(this.props.dataSource);
-    }
-
-    renderOption(item) {
-        return (
-            <Option key={item.key} text={item.description} >
-                {this.getTextItem(item)}
-            </Option>
-        );
-    }
-
-    getOutData(outData, item) {
-        return outData.map((field) => {
-            return item[field];
-        });
-    }
-
-    getTextItem(item) {
-        return this.props.displayedfields.map((field) => {
-            return item[field] + ' | ';
-        }); 
-    }
-
-    setData(dataSource) {
-        if(dataSource.map != undefined) {
-            let dataSource = dataSource.map(this.renderOption);
-            this.setState({data: dataSource});
-        }
-    }
-
-    render() {
-
-        const { classes } = this.props;
-
-        return ( 
-            <AutoComplete
-                  className={this.props.className || 'default'}
-                  size={this.props.size}
-                  style={{ width: '100%' }}
-                  disabled={this.props.disabled}
-                  dataSource={this.state.data}
-                  onSelect={this.props.onSelect}
-                  onSearch={this.props.handleSearch}
-                  value={this.props.value}
-                  optionLabelProp="text"
-                >
-                    <JlInput
-                        label={this.props.label}
-                        value={this.props.value}
-                        disabled={this.props.disabled}
-                        fullWidth
-                        margin="normal"
-                        variant="outlined"
-                    />
-            </AutoComplete>
-        );
-    }
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) =>
+          part.highlight ? (
+            <span key={String(index)} style={{ fontWeight: 500 }}>
+              {part.text}
+            </span>
+          ) : (
+            <strong key={String(index)} style={{ fontWeight: 300 }}>
+              {part.text}
+            </strong>
+          ),
+        )}
+      </div>
+    </MenuItem>
+  );
 }
 
-export default withStyles(styles)(JlAutoComplete);
+function getSuggestions(value) {
+  const inputValue = deburr(value.trim()).toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
+
+  return inputLength === 0
+    ? []
+    : suggestions.filter(suggestion => {
+        const keep =
+          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+        if (keep) {
+          count += 1;
+        }
+
+        return keep;
+      });
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.label;
+}
+
+const styles = theme => ({
+  root: {
+    height: 250,
+    flexGrow: 1,
+  },
+  container: {
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  cssLabel: {
+    '&$cssFocused': {
+      color: blue[400],
+    },
+  },
+  disabled: {
+      borderColor: '#000',
+      borderBottomColor: '#000',
+      color: '#595959'
+  },
+  cssFocused: {
+      fontSize: 14
+  },
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: blue[400],
+    },
+  },
+  cssOutlinedInput: {
+      color: '#000',
+      fontSize: 14,
+    '&$cssFocused $notchedOutline': {
+      borderColor: blue[400],
+    },
+  },
+  notchedOutline: {
+      fontSize: 14
+  },
+  bootstrapRoot: {
+    'label + &': {
+      marginTop: theme.spacing.unit * 3,
+    },
+  },
+});
+
+class AutoComplete extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.renderInputComponent = this.renderInputComponent.bind(this);
+  }
+
+  state = {
+    single: '',
+    popper: '',
+    suggestions: [],
+  };
+
+  renderInputComponent(inputProps) {
+    const { classes, inputRef = () => {}, ref, ...other } = inputProps;
+  
+    return (
+      <TextField
+        fullWidth
+        variant="outlined"
+        label={this.props.label}
+        className={this.props.className}
+        InputProps={{
+          inputRef: node => {
+            ref(node);
+            inputRef(node);
+          },
+          classes: {
+            input: classes.input,
+            classes: {
+              root: classes.cssOutlinedInput,
+              focused: classes.cssFocused,
+              disabled: classes.disabled,
+              notchedOutline: classes.notchedOutline,
+            },
+          },
+        }}
+        {...other}
+      />
+    );
+  }  
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    request.get('clients/districts')
+      .then((response) =>  {
+        this.setState({ suggestions: response.data })
+      });
+  }
+
+  onSuggestionsClearRequested = () => {
+    this.setState({ suggestions: [] });
+  };
+
+  handleChange = name => (event, { newValue }) => {
+    this.setState({
+      [name]: newValue,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    const autosuggestProps = {
+      renderInputComponent: this.renderInputComponent,
+      suggestions: this.state.suggestions,
+      onSuggestionsFetchRequested: this.onSuggestionsFetchRequested,
+      onSuggestionsClearRequested: this.onSuggestionsClearRequested,
+      getSuggestionValue,
+      renderSuggestion,
+    };
+
+    return (
+      <div className={classes.root}>
+        <Autosuggest
+          {...autosuggestProps}
+          inputProps={{
+            classes,
+            placeholder: 'Search a country (start with a)',
+            value: this.state.single,
+            onChange: this.handleChange('single'),
+          }}
+          theme={{
+            container: classes.container,
+            suggestionsContainerOpen: classes.suggestionsContainerOpen,
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion,
+              root: classes.cssOutlinedInput,
+              focused: classes.cssFocused,
+              disabled: classes.disabled,
+              notchedOutline: classes.notchedOutline,
+          }}
+          renderSuggestionsContainer={options => (
+            <Paper {...options.containerProps} square>
+              {options.children}
+            </Paper>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(AutoComplete);
