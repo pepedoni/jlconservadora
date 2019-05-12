5f2407a346937472eb55d7c902b26584c9a5b2bb
pepedoni <phpedromoutinho@gmail.com>
2019-05-12 18:01:31 -0300

    DashBoard
    Card proximos aniversarios

---
+++ frontend/src/features/dashboard/birthdaysMonth.tsx
@@ -0,0 +1,72 @@
+import { Card } from 'antd';
+import React, { Component } from 'react'
+
+import { bindActionCreators } from "redux";
+import { Row, Col, Button } from "antd";
+import { connect } from "react-redux";
+import moment from 'moment';
+
+class BirthdaysMonth extends Component {
+
+    constructor(props) {
+        
+        super(props);
+
+        var date = new Date();
+
+        this.ano = date.getFullYear();
+        this.aniversariantes = [{date: "15/05", name: "Pedro"}, {date: "21/05", name: "João"}];
+
+    }
+
+    getAniversariante(aniversariante) {
+
+        let aniversario = aniversariante.date + "/" + this.ano;
+
+        return (
+            <Row gutter={16} style={{ paddingLeft: '30px'}}>
+                <Col className="gutter-row" md={20} span={8}>
+                    {aniversariante.name}
+                </Col>
+                <Col className="gutter-row" md={4} span={8}>
+                    {this.calculaDiferencaDatas(aniversario) + "  dias"}
+                </Col>
+            </Row>
+        );
+    }
+
+    calculaDiferencaDatas(date1){
+        //formato do brasil 'pt-br'
+        moment.locale('pt-br');
+        //setando data1
+        var data1 = moment(date1,'DD/MM/YYYY');
+        //setando data2
+        var data2 = moment(new Date());
+        //tirando a diferenca da data2 - data1 em dias
+        var diff  = data2.diff(data1, 'days');
+        
+        return Math.abs(diff) + 1;
+    }
+
+
+    render() {
+        return (
+            <Card>
+                <Card title="Próximos Aniversarios" bordered={false}></Card>
+                { this.aniversariantes.map(aniversariante => this.getAniversariante(aniversariante)) }
+            </Card>
+        )
+      }
+
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {},
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(BirthdaysMonth);
+
--- frontend/src/features/dashboard/dashboardContainer.tsx
+++ frontend/src/features/dashboard/dashboardContainer.tsx
@@ -1,28 +1,19 @@
-import { Card } from 'antd';
 import React, { Component } from 'react'
 
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-
-// import { LineChart } from "react-chartjs";
-
-const dailySalesChart = { 
-  data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    series: [[12, 17, 7, 17, 23, 18, 38]]
-  }
-};
+import BirthdaysMonth from './birthdaysMonth';
+import { Row, Col } from "antd";
 
 class Dashboard extends Component {
     render() {
         return (
           <div>
-
-              <Card>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-              </Card>
+            <Row gutter={8}>
+              <Col className="gutter-row" md={8}>
+                <BirthdaysMonth></BirthdaysMonth>
+              </Col>
+            </Row>
           </div>
         )
       }
